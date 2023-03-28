4aafcf585bfe9a8010a7a6ad5aa743d554e01358
ofkTuhin <tuhin.omar99@gmail.com>
2023-03-28 08:45:34 +0600

    fixed error

--- layouts/Sidebar.js
+++ layouts/Sidebar.js
@@ -46,54 +46,56 @@ const Sidebar = ({
   };
 
   const { data: session } = useSession();
-  const { role } = session?.user?.user;
+
   const SidebarContent = (
     <Box p={2} height="100%">
       <Logo />
       <Box mt={2}>
         <List>
-          {Menuitems.filter((item) => item[role]).map((item, index) => (
-            <List component="li" disablePadding key={item.title}>
-              <Button
-                // href={updatedButton ? item.href : ""}
-                onClick={() => handleRoute(item.href)}
-                sx={{
-                  display: "block",
-                  width: "100%",
-                }}
-              >
-                <ListItem
-                  onClick={() => handleClick(index, item.value)}
-                  button
-                  selected={location === item.href}
+          {Menuitems.filter((item) => item[session?.user?.user?.role]).map(
+            (item, index) => (
+              <List component="li" disablePadding key={item.title}>
+                <Button
+                  // href={updatedButton ? item.href : ""}
+                  onClick={() => handleRoute(item.href)}
                   sx={{
-                    mb: 1,
-                    ...(location === item.href && {
-                      color: "white",
-                      width: "100%",
-                      backgroundColor: (theme) =>
-                        `${theme.palette.primary.main}!important`,
-                    }),
+                    display: "block",
+                    width: "100%",
                   }}
                 >
-                  <ListItemIcon>
-                    <FeatherIcon
-                      style={{
-                        color: `${location === item.href ? "white" : ""} `,
-                      }}
-                      icon={item.icon}
-                      width="20"
-                      height="20"
-                    />
-                  </ListItemIcon>
+                  <ListItem
+                    onClick={() => handleClick(index, item.value)}
+                    button
+                    selected={location === item.href}
+                    sx={{
+                      mb: 1,
+                      ...(location === item.href && {
+                        color: "white",
+                        width: "100%",
+                        backgroundColor: (theme) =>
+                          `${theme.palette.primary.main}!important`,
+                      }),
+                    }}
+                  >
+                    <ListItemIcon>
+                      <FeatherIcon
+                        style={{
+                          color: `${location === item.href ? "white" : ""} `,
+                        }}
+                        icon={item.icon}
+                        width="20"
+                        height="20"
+                      />
+                    </ListItemIcon>
 
-                  <ListItemText onClick={onSidebarClose}>
-                    {item.title}
-                  </ListItemText>
-                </ListItem>
-              </Button>
-            </List>
-          ))}
+                    <ListItemText onClick={onSidebarClose}>
+                      {item.title}
+                    </ListItemText>
+                  </ListItem>
+                </Button>
+              </List>
+            )
+          )}
         </List>
       </Box>
     </Box>
--- pages/skills.js
+++ pages/skills.js
@@ -1,4 +1,5 @@
 import FullLayout from "@layouts/FullLayout";
+import { getSession } from "next-auth/react";
 import React from "react";
 
 const Skills = () => {
@@ -10,3 +11,19 @@ const Skills = () => {
 };
 
 export default Skills;
+export async function getServerSideProps(context) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth/signIn",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: { session },
+  };
+}
